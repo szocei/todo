250ca68581d7f93dcde9a4d45fc69a4a13918bf8
szocei <szocei.zoltan@gmail.com>
2020-12-08 21:42:08 +0100

    felig kesz

---
+++ todo2.js
@@ -0,0 +1,52 @@
+checkTodos();
+//date
+(function date(){
+    const nowDate=new Date();
+    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    document.querySelector('.div__day').textContent=days[nowDate.getDay()];
+    document.querySelector('.div__date').textContent=`${nowDate.getDate()}-${nowDate.getMonth()}-${nowDate.getFullYear()}`;
+}
+)();
+
+function checkTodos() {
+  let dataInLocalStorage = localStorage.getItem("todos");
+   
+  if (dataInLocalStorage == null) {
+    todos = [];
+    document.querySelector('.db').innerHTML='0';
+  } else {
+    todos = JSON.parse(dataInLocalStorage);
+  }
+  let html = "";
+  todos.forEach((todo, index) => {
+    html += `<div class='card'><div class='deldiv' onclick='removeTodo(${index});'>X</div>${todo}</div>`;
+  });
+  $(".incomplete").empty().append(html);
+  document.querySelector('.db').innerHTML=todos.length;
+};
+
+
+function letrehoz (){
+ if ($("input").val() !== "") {
+      todo = $("input").val();
+      let todosData = localStorage.getItem("todos");
+      if (todosData == null) {
+        todos = [];
+      } else {
+        todos = JSON.parse(todosData);
+      }
+      todos.push(todo);
+      localStorage.setItem("todos", JSON.stringify(todos));
+      $("input").val("");
+      checkTodos();
+    }
+};
+
+  let removeTodo = (index) => {
+    let todosData = localStorage.getItem("todos");
+    todos = JSON.parse(todosData);
+    todos.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    checkTodos();
+  };
+
